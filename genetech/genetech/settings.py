cc9aa844c67fadc7d73d2750467d27951f9528a7
kalokola <brightiuskalokola@gmail.com>
2023-09-22 22:04:39 +0200

    Weledi

--- genetech/genetech/settings.py
+++ genetech/genetech/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6q^w(psm__(xgvzw!#zvwh3raali$oe(z=wxqr@p(lk4ypfm)d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['137.184.229.202','gene.zipa.tech', '127.0.0.1','localhost']
 
 
 # Application definition
