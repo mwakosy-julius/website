43301a4e0be222d44db16acc9e15477f483a54ee
kalokola <brightiuskalokola@gmail.com>
2023-09-22 21:53:01 +0200

    Weledi

--- genetech/genetech/urls.py
+++ genetech/genetech/urls.py
@@ -1,7 +1,10 @@
+from django.views.static import serve
 from django.contrib import admin
-from django.urls import path, include
-
+from django.urls import path, re_path, include
+from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('website.urls')),
+
+    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
