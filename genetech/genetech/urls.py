c3a4b3f52573b340c9cd62fd482c9e9164573130
mwakosy-julius <mwakosyajulius619@gmail.com>
2024-02-21 13:42:58 +0300

    new kaidoku

--- genetech/genetech/urls.py
+++
@@ -1,9 +0,0 @@
-from django.views.static import serve
-from django.contrib import admin
-from django.urls import path, re_path, include
-from django.conf import settings
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('website.urls')),
-    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
-]
